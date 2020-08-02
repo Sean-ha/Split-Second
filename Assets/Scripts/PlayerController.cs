1fd8d555ad56535fba5a9eb00684e487b5e7de0b
Sean <seanha2013@gmail.com>
2020-08-01 20:27:41 -0400

    Basic character movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpForce;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rigidBody;
+    private Transform overlapBoxTransform;
+
+    private bool canMove;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        overlapBoxTransform = transform.Find("BoxOverlap");
+
+        canMove = true;
+    }
+
+    void Update()
+    {
+        if(canMove)
+        {
+            float horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+            if(IsGrounded())
+            {
+                CheckJump();
+            }
+
+            rigidBody.velocity = new Vector2(horizontalDirection * movementSpeed, rigidBody.velocity.y);
+        }
+    }
+
+    // Checks if the user is grounded. Called every frame.
+    private bool IsGrounded()
+    {
+        Collider2D overlap = Physics2D.OverlapBox(overlapBoxTransform.position, new Vector2(0.4f, 0.02f), 0f, 
+            whatIsGround);
+
+        if(overlap != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Checks if the user pressed the jump key while grounded. Called every frame.
+    private void CheckJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    public void SetCanMove(bool value)
+    {
+        canMove = value;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
