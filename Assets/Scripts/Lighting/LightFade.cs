c357d9c721752a7cbe7e684f75c47e9f2a57ddd3
Sean <seanha2013@gmail.com>
2020-08-04 17:13:34 -0400

    More post processing. Particle lights.

---
+++ Assets/Scripts/Lighting/LightFade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightFade : MonoBehaviour
+{
+    private Light2D light2D;
+
+    void Start()
+    {
+        light2D = GetComponent<Light2D>();
+        StartCoroutine(Fader());
+    }
+
+    IEnumerator Fader()
+    {
+        float time = 0.75f;
+
+        while(time > 0)
+        {
+            light2D.intensity = (time / 0.75f) * .2f;
+            time -= Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
